11380a37f348226ebf5858f8e69c7129d1c6f54a
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-12 11:02:15 +1200

    Reset to python-template https://github.com/cmw278/python-template

--- Makefile
+++
@@ -1,9 +0,0 @@
-# 
-init:
-	pip install pipenv
-	pipenv install --dev
-	pipenv run autopep8 -riaa ./src
-
-# commands to run tests go here
-test:
-	pipenv run pycodestyle ./src
--- src/example.py
+++
@@ -1,6 +0,0 @@
-import math, string
-
-# This file serves to demonstrate the autopep8 formatter used with this project's CI processes
-class SomeClass:
-  def doSomething(self):
-    pass
