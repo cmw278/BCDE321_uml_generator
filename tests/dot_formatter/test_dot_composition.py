69b66e07fc7f7f9e1a7b50551d1783b44d30790f
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-18 00:00:35 +1200

    Removed skipping from DotRelationship test classes

--- tests/dot_formatter/test_dot_composition.py
+++ tests/dot_formatter/test_dot_composition.py
@@ -9,8 +9,6 @@ from test_data import DATA
 class TestDotComposition(TestDotInheritance):
 
     def setUp(self):
-        # TODO: Implement DotComposition
-        self.skipTest('Awaiting implementation of DotComposition')
         source_class = DATA['relationship']['source_class']
         target_class = DATA['relationship']['target_class']
         label = DATA['relationship']['label']
