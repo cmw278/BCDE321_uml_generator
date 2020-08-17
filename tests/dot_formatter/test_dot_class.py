a419a5cb021c93e68e54ad6acc4cc6c02e14d144
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-17 23:56:29 +1200

    Refactored tests for better re-use

--- tests/dot_formatter/test_dot_class.py
+++ tests/dot_formatter/test_dot_class.py
@@ -4,10 +4,6 @@ from dot_formatter import (DotClass, DotObject, DotAttribute,
 
 # Test data dictionary
 from test_data import DATA
-_METHOD = DATA['method']
-_ATTRIBUTE = DATA['attribute']
-_RELATIONSHIP = DATA['relationship']['association']
-_CLASS_NAME = DATA['class_name']
 
 
 class TestDotClass(TestCase):
@@ -15,7 +11,17 @@ class TestDotClass(TestCase):
     def setUp(self):
         # TODO: Implement DotClass
         self.skipTest('Awaiting implementation of DotClass')
-        self.class_ = DotClass(_CLASS_NAME)
+        class_name = DATA['class_name']
+        method = DATA['method']
+        attribute = DATA['attribute']
+        target_class = DATA['relationship']['target_class']
+        self.test_data = {
+            'class_name': class_name,
+            'method': method,
+            'attribute': attribute,
+            'target_class': target_class
+        }
+        self.class_ = DotClass(class_name)
 
     def test_inheritance(self):
         expected = DotObject
@@ -23,42 +29,45 @@ class TestDotClass(TestCase):
         self.assertIsInstance(expected, actual)
 
     def test_class__name(self):
-        expected = _CLASS_NAME
+        expected = test_data['class_name']
         actual = self.class_.name
         self.assertEqual(actual, expected)
 
     def add_attribute(self):
         return self.class_.add_attribute(
-            _ATTRIBUTE['name'],
-            _ATTRIBUTE['type']
+            self.test_data['attribute']['name'],
+            self.test_data['attribute']['type']
         )
 
     def test_add_attribute(self):
-        expected = DotAttribute(_ATTRIBUTE['name'], _ATTRIBUTE['type'])
+        name = self.test_data['attribute']['name']
+        type_ = self.test_data['attribute']['type']
+        expected = DotAttribute(name, type_)
         actual = self.add_attribute()
         self.assertEqual(actual, expected)
 
     def add_method(self):
         return self.class_.add_method(
-            _METHOD['name'],
-            _METHOD['return_type']
+            self.test_data['method']['name'],
+            self.test_data['method']['return_type']
         )
 
     def test_add_method(self):
-        expected = DotMethod(_METHOD['name'], _METHOD['return_type'])
+        name = self.test_data['method']['name']
+        type_ = self.test_data['method']['type']
+        expected = DotMethod(name, type_)
         actual = self.add_method()
         self.assertEqual(actual, expected)
 
     def add_relationship(self):
         return self.class_.add_relationship(
-            _RELATIONSHIP['type'],
-            _RELATIONSHIP['target_class']
+            self.test_data['target_class']
         )
 
     def test_add_relationship(self):
         expected = DotRelationship(
-            _CLASS_NAME,
-            _RELATIONSHIP['target_class']
+            self.test_data['class_name'],
+            self.test_data['target_class']
         )
         actual = self.add_relationship()
         self.assertEqual(actual, expected)
