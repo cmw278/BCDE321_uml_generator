a358758011fa12ae2d813170984ef30f3ce69ac2
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-17 23:58:35 +1200

    Implemented DotRelationship classes + refactored DotAssociation class to conform to improved structure

--- src/dot_formatter/dot_association.py
+++ src/dot_formatter/dot_association.py
@@ -1,18 +1,20 @@
 class DotAssociation:
 
-    def __init__(self, class_: str, target_class: str, label: str = ''):
-        self.my_class = class_
+    def __init__(self, source_class: str, target_class: str, label: str = ''):
+        self.source_class = source_class
         self.target_class = target_class
-        self.label = label
+        self._set_options(label)
+
+    def _set_options(self, label: str) -> None:
+        self.options = {
+            'label': label
+        }
 
     def __str__(self):
-        return ('\n%s -> %s' % (self.target_class, self.my_class)
+        return ('\n%s -> %s' % (self.target_class, self.source_class)
                 + self._get_options())
 
     def _get_options(self) -> str:
         return ' [\n%s\n]' % '\n'.join(
-            ['%s="%s"' % (key, value) for key, value in self._options()]
+            ['%s="%s"' % (key, value) for key, value in self.options.items()]
         )
-
-    def _options(self):
-        yield ('label', self.label)
--- src/dot_formatter/dot_composition.py
+++ src/dot_formatter/dot_composition.py
@@ -1,5 +1,9 @@
-from dot_formatter import DotAssociation
+from dot_formatter import DotAggregation
 
 
-class DotComposition(DotAssociation):
-    pass
+class DotComposition(DotAggregation):
+
+    def _set_options(self, label: str, arrowtail: str = 'diamond') -> None:
+        # Override so that arrowtail defaults to `diamond` (filled diamond)
+        # for composition relationship.
+        return super()._set_options(label, arrowtail)
