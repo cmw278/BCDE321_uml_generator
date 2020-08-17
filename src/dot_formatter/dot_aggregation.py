c834962dda344bf29b89fdf8527ebd5aff558be3
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-17 23:58:35 +1200

    Implemented DotRelationship classes + refactored DotAssociation class to conform to improved structure

--- src/dot_formatter/dot_aggregation.py
+++ src/dot_formatter/dot_aggregation.py
@@ -1,5 +1,15 @@
-from dot_formatter import DotAssociation
+from dot_formatter import DotInheritance
 
 
-class DotAggregation(DotAssociation):
-    pass
+class DotAggregation(DotInheritance):
+
+    def __init__(self, source_class: str, target_class: str, label: str = ''):
+        # Invert source and target for aggregation relationship.
+        # i.e. Source contains target, so __str__() output is like:
+        # `source -> target`.
+        super().__init__(target_class, source_class, label)
+
+    def _set_options(self, label: str, arrowtail: str = 'ediamond') -> None:
+        # Override so that arrowtail defaults to `ediamond` (empty diamond)
+        # for composition relationship.
+        return super()._set_options(label, arrowtail)
--- src/dot_formatter/dot_inheritance.py
+++ src/dot_formatter/dot_inheritance.py
@@ -2,4 +2,11 @@ from dot_formatter import DotAssociation
 
 
 class DotInheritance(DotAssociation):
-    pass
+
+    def _set_options(self, label: str, arrowtail: str = 'empty') -> None:
+        # Override to introduce the arrowtail option, which defaults
+        # to `empty` (arrow outline) for inheritance relationship.
+        self.options = {
+            'label': label,
+            'arrowtail': arrowtail
+        }
