73ea6496b5acac2cc827e1600e4c6d762f467fb0
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-18 17:46:55 +1200

    Modified __init__.py + changed import ordering to resolve import error

--- src/dot_formatter/__init__.py
+++ src/dot_formatter/__init__.py
@@ -3,8 +3,10 @@ from .dot_object import DotObject
 from .dot_argument import DotArgument
 from .dot_attribute import DotAttribute
 from .dot_method import DotMethod
-from .dot_class import DotClass
+from .dot_relationship import DotRelationship
 from .dot_association import DotAssociation
 from .dot_inheritance import DotInheritance
 from .dot_aggregation import DotAggregation
 from .dot_composition import DotComposition
+from .uml_relationship import UMLRelationship
+from .dot_class import DotClass
