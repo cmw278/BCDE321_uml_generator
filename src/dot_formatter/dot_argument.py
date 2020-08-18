e8a1643676faf47de67ceff94e2ebe350d749507
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-18 13:17:38 +1200

    Implement DotArgument and DotAttribute - Run tests

--- src/dot_formatter/dot_argument.py
+++ src/dot_formatter/dot_argument.py
@@ -2,4 +2,10 @@ from dot_formatter import DotObject
 
 
 class DotArgument(DotObject):
-    pass
+
+    def __init__(self, name: str, type_: str):
+        super().__init__(name)
+        self.type = type_
+
+    def __str__(self) -> str:
+        return '%s: %s' % (self.name, self.type)
