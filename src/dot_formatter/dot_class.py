38bb6e53994811cf50564750eb5cb83eec402a7d
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-18 18:56:24 +1200

    Implemented DotClass + changed DotRelationship's inheritance to gain __eq__() and __hash__() + enabled TestDotClass unit tests

--- src/dot_formatter/dot_class.py
+++ src/dot_formatter/dot_class.py
@@ -1,5 +1,46 @@
-from dot_formatter import DotObject, DotAttribute, DotMethod
+from dot_formatter import (DotObject, DotAttribute, DotMethod,
+                           DotRelationship, UMLRelationship)
 
 
 class DotClass(DotObject):
-    pass
+
+    def __init__(self, name):
+        super().__init__(name)
+        self.all_my_attributes = []
+        self.all_my_methods = []
+        self.all_my_relationships = []
+
+    def add_attribute(self, attribute_name: str,
+                      attribute_type: str) -> DotAttribute:
+        new_attribute = DotAttribute(attribute_name, attribute_type)
+        self.all_my_attributes.append(new_attribute)
+        return new_attribute
+
+    def add_method(self, method_name: str,
+                   method_return_type: str) -> DotMethod:
+        new_method = DotMethod(method_name, method_return_type)
+        self.all_my_methods.append(new_method)
+        return new_method
+
+    def add_relationship(self, relationship_type: UMLRelationship,
+                         target_class: str,
+                         label: str = '') -> DotRelationship:
+        """relationship_type is an Enum instance that references
+        DotRelationship subclasses. relationship_type.make(*args)
+        instantiates the relevant class using the default constructor."""
+        new_relationship = (relationship_type
+                            .make(self.name, target_class, label))
+        self.all_my_relationships.append(new_relationship)
+        return new_relationship
+
+    def __str__(self):
+        return ('%s [\nlabel=<{<B>\\N</B>|%s|%s}>\n]\n%s'
+                % (
+                    self.name,
+                    ''.join([str(an_attribute) for an_attribute
+                             in self.all_my_attributes]),
+                    ''.join([str(a_method) for a_method
+                             in self.all_my_methods]),
+                    '\n'.join([str(a_relationship) for a_relationship
+                               in self.all_my_relationships])
+                ))
