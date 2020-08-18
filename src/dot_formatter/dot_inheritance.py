4ec3b585e06804919c5ce4a6df17716d903515ac
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-18 17:44:13 +1200

    Refactor DotRelationship classes + introduced DotRelationship abstract class + introduced UMLRelationships Enumerator for DotRelationship interface

--- src/dot_formatter/dot_inheritance.py
+++ src/dot_formatter/dot_inheritance.py
@@ -1,11 +1,9 @@
-from dot_formatter import DotAssociation
+from dot_formatter import DotRelationship
 
 
-class DotInheritance(DotAssociation):
+class DotInheritance(DotRelationship):
 
     def _set_options(self, label: str, arrowtail: str = 'empty') -> None:
-        # Override to introduce the arrowtail option, which defaults
-        # to `empty` (arrow outline) for inheritance relationship.
         self.options = {
             'label': label,
             'arrowtail': arrowtail
