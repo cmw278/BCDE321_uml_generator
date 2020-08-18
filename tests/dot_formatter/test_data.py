5d6884df94cbca731cbccfa84ce17d5d8d923cb6
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-18 23:28:42 +1200

    Updated test-data

--- tests/dot_formatter/test_data.py
+++ tests/dot_formatter/test_data.py
@@ -2,6 +2,10 @@ import os
 import json
 
 _PATH = os.path.dirname(__file__)
-_JSON_FILE = '/test-data.json'
+_JSON_FILE = '/test-data/dot-formatter.json'
 
 DATA = json.load(open(_PATH + _JSON_FILE))
+
+_JSON_TOYBOX_TOY = '/test-data/toybox-toy.json'
+
+TOYBOX_TOY = json.load(open(_PATH + _JSON_TOYBOX_TOY))
