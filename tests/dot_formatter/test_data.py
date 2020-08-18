41452b394063043433c2e34a621ba6a88cad27dd
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-19 00:07:21 +1200

    Added sample output for ToyboxToy + improved test_data.py

--- tests/dot_formatter/test_data.py
+++ tests/dot_formatter/test_data.py
@@ -1,11 +1,21 @@
 import os
 import json
+import gc
 
 _PATH = os.path.dirname(__file__)
-_JSON_FILE = '/test-data/dot-formatter.json'
 
-DATA = json.load(open(_PATH + _JSON_FILE))
+_FILE_NAME = '/test-data/dot-formatter.json'
+DATA = json.load(open(_PATH + _FILE_NAME))
 
-_JSON_TOYBOX_TOY = '/test-data/toybox-toy.json'
+_FILE_NAME = '/test-data/toybox-toy.json'
+TOYBOX_TOY = json.load(open(_PATH + _FILE_NAME))
 
-TOYBOX_TOY = json.load(open(_PATH + _JSON_TOYBOX_TOY))
+_FILE_NAME = '/test-data/toybox-toy.gv'
+_FILE = open(_PATH + _FILE_NAME)
+EXPECTED_OUTPUT = str(_FILE.read())
+_FILE.close()
+
+del _PATH
+del _FILE_NAME
+del _FILE
+gc.collect()
