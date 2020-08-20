37a10bb0b16cf1ab2ee6fe44816658e2640fd01c
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-20 16:52:02 +1200

    Minor test updates to test_dot_formatter.py + changed import order in dot_formatter/__init__.py + modified toybox-toy.json

--- src/dot_formatter/__init__.py
+++ src/dot_formatter/__init__.py
@@ -1,4 +1,3 @@
-from .dot_formatter import DotFormatter
 from .dot_object import DotObject
 from .dot_argument import DotArgument
 from .dot_attribute import DotAttribute
@@ -10,3 +9,4 @@ from .dot_aggregation import DotAggregation
 from .dot_composition import DotComposition
 from .uml_relationship import UMLRelationship
 from .dot_class import DotClass
+from .dot_formatter import DotFormatter
