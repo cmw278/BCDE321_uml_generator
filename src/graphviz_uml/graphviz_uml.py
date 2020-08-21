03b14c34a4389a8441647a8f1220e2c26810595e
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-21 22:53:16 +1200

    Improved formatting in graphviz_uml.py

--- src/graphviz_uml/graphviz_uml.py
+++ src/graphviz_uml/graphviz_uml.py
@@ -11,92 +11,68 @@ class GraphvizUML(Source, DotFormatter):
     using both the DotFormatter API AND the graphviz.Source API, although
     some behaviour is altered.
 
-    All altered behaviour is documented in the method docstrings."""
+    All altered behaviour is documented in the method docstrings.
+    """
 
-    def __init__(
-        self,
-        system_dict: dict,
-        *,
-        template_path: str = None,
-        filename: str = None,
-        directory: str = None,
-        format: str = 'png'
-    ):
+    def __init__(self, system_dict: dict, *, template_path: str = None,
+                 filename: str = None, directory: str = None,
+                 format: str = 'png'):
         """Creates a GraphvizUML instance that may be interacted with using
         the API of graphviz.Source AND dot_formatter.DotFormatter.
         DotFormatter is instantiated first, then cast as string to instantiate
         Source.
 
         ---
-
         Required Positional Arguments:
 
-        [0] system_dict: dict  -> A dictionary representation of the system
-        you are creating a UML model for.
+        `[0] system_dict: dict`
+        > A dictionary representation of the system you are creating a UML
+        model for.
 
         ---
-
         Optional Keyword Arguments:
 
-        template_path: str  -> Path to a custom DOT file template, used when
-        casting DotFormatter as a string.
+        `template_path: str`
+        > Path to a custom DOT file template, used when casting DotFormatter
+        as a string.
 
-        filename: str  -> Filename for saving the output. Defaults to
-        system_dict['folder_name'].
+        `filename: str`
+        > Filename for saving the output. Defaults to
+        `system_dict['folder_name']`.
 
-        directory: str  –> (Sub)directory for source saving and rendering.
+        `directory: str`
+        > (Sub)directory for source saving and rendering.
 
-        format: str  –> Rendering output format ('pdf', 'png', …). Defaults
-        to 'png' (different behaviour from graphviz.Source).
+        `format: str`
+        > Rendering output format ('pdf', 'png', …). Defaults
+        to `png` (different behaviour from graphviz.Source).
         """
         assert (format in FORMATS,
-                'Invalid format: %s\nFormat must be one of: [\n\t%s\n]'
-                % (
-                    format,
-                    ',\n\t'.join(["'%s'" % format_ for format_ in FORMATS])
-                ))
-        DotFormatter.__init__(
-            self,
-            system_dict,
-            template_path=template_path
-        )
-        Source.__init__(
-            self,
-            str(self),
-            filename=self.name if filename is None else filename,
-            directory=directory,
-            format=format
-        )
+                '%s format: is not supported. Format must be one of: [ %s ]'
+                % (format, ', '.join(FORMATS)))
+        DotFormatter.__init__(self, system_dict, template_path=template_path)
+        Source.__init__(self, str(self),
+                        filename=self.name if filename is None else filename,
+                        directory=directory, format=format)
 
-    def view(
-        self,
-        *,
-        filename: str = None,
-        directory: str = None,
-        cleanup: bool = True,
-        format: str = None
-    ):
+    def view(self, *, filename: str = None, directory: str = None,
+             cleanup: bool = True, format: str = None):
         """Save the UML diagram to file, open the result in a viewer.
 
         ---
-
         Optional Keyword Arguments:
 
-        filename – Filename for saving the source (defaults to name + '.gv')
+        `filename: str`
+        > Filename for saving the source (defaults to name + '.gv')
 
-        directory – (Sub)directory for source saving and rendering.
+        `directory: str`
+        > (Sub)directory for source saving and rendering.
 
-        cleanup (bool) – Delete the source file after rendering.
-        (Defaults to True)
+        `cleanup: bool`
+        > Delete the source file after rendering (Defaults to True).
 
-        format – The output format used for rendering
-        ('pdf', 'png', etc.).
+        `format: str`
+        > The output format used for rendering (pdf, png, etc.).
         """
-        Source.render(
-            self,
-            view=True,
-            filename=filename,
-            directory=directory,
-            cleanup=cleanup,
-            format=format
-        )
+        Source.render(self, view=True, filename=filename,
+                      directory=directory, cleanup=cleanup, format=format)
