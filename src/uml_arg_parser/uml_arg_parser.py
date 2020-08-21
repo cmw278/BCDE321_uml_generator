257c69708493e3d44ca8e06f4172095efd95f0bd
cmw278 <chris.wordsworth@outlook.co.nz>
2020-08-21 23:14:25 +1200

    Starting f3: arg-parser

---
+++ src/uml_arg_parser/uml_arg_parser.py
@@ -0,0 +1,8 @@
+from argparse import ArgumentParser
+
+
+class UMLArgParser(ArgumentParser):
+    """A wrapper for ArgumentsParser that defines and processes arguments for
+    `uml-generator`
+    """
+    pass
