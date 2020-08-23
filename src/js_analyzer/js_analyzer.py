3f8528ec55000e45d87d3dd2c48c1f62240d6e08
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2020-08-23 17:45:25 +1200

    E thing

--- src/js_analyzer/js_analyzer.py
+++ src/js_analyzer/js_analyzer.py
@@ -8,13 +8,13 @@ class JsAnalyzer:
 
     def load_file(self, filepath: str) -> None:
         file_ = open(filepath)
-        data = esprima.parse(file_.read())
+        data_ = esprima.parse(file_.read())
         file_.close()
         del file_
-        self.load_data(data)
+        self.load_data(data_)
 
     def load_data(self, data_: dict) -> None:
         if 'body' in data_:
             self._classes.append(
-                JsClass(class_dict) for class_dict in
-                data_['body'] if class_dict['type'] == 'ClassDeclaration')
+                JsClass(class_data) for class_data in
+                data_['body'] if class_data['type'] == 'ClassDeclaration')
