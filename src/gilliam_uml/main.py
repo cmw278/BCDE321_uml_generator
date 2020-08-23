386c377f8c91acfbd8f107a9531c5f79ea4325ae
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2020-08-23 16:24:11 +1200

    Update main.py

--- src/gilliam_uml/main.py
+++ src/gilliam_uml/main.py
@@ -4,23 +4,35 @@ from .controller import GilliamController
 
 
 class GilliamPrompt(Cmd):
-    doc_header = "Here are the list of commands in help.\n To get help on a command enter 'help" \
-                 " followed by command name'."
+    doc_header = "Here are the list of commands in help.\n To get help on a "\
+     "command, enter 'help' followed by command name'."
 
     def do_help_list(self, arg):
-        """Enter 'help_list to see a list of all the commands in help and what they do."""
-        print(
-            "analyzer\n\tEnter 'analyzer' to analysis the selected file.\n"
-            "\ndraw\n\tEnter 'draw' to draw the selected file.\n"
-            "\nhelp_list\n\tEnter 'help_list to see a list of all the commands in help and what they do.\n"
-            "\nselect_file\n\tEnter 'select_file' to enter the file path of the JavaScript file that" +
-            "requires a UML diagram\n "
-            "\nshut\n\tEnter 'shut y' To leave the program.")
+        """Enter 'help_list to see a list of all the commands in help """ \
+          """and what they do."""
+        print("analyzer\t\tEnter 'analyzer' to analysis the selected file.\n"
+              "\ndraw\t\tEnter 'draw' to draw the selected file.\n"
+              "\nhelp_list\t\tEnter 'help_list to see a list of all the " +
+              "commands in help and what they do.\n"
+              "\nselect_file\t\tEnter 'select_file' to enter the file " +
+              "path of the JavaScript file that requires a UML diagram.\n"
+              "\nshut\t\tEnter 'shut y' To leave the program.")
 
     def do_select_file(self, arg):
-        """Enter 'select_file' to enter the file path of the JavaScript file that requires a UML diagram """
-        print('Hello')
+        """Enter 'select_file' to enter the file path of the JavaScript"""\
+         """file that requires a UML diagram """
+        print('Enter the file path and file name of the file that ' +
+              'requires analyzing')
         # call file opener
+
+        file_name = arg
+        #self.do_open_file(file_name)
+        print(file_name)
+
+    def do_open_file(self, file_name):
+        print("Hi", file_name)
+        file = open(file_name)
+        print(file.read())
 
     def do_analyzer(self, arg):
         """Enter 'analyzer' to analysis the selected file."""
@@ -34,11 +46,7 @@ class GilliamPrompt(Cmd):
 
     def do_shut(self, args):
         """ Enter 'shut y' To leave the program."""
-        print("Enter' Confirm Y' to close program. To leave program open enter 'Confirm N")
-
-    def do_Confirm(self, args):
-        """Close the program."""
-        if args == "Y":
+        if args == "y":
             sys.exit()
         else:
             print("Welcome back. Enter a command!")
@@ -47,5 +55,6 @@ class GilliamPrompt(Cmd):
 if __name__ == '__main__':
     prompt = GilliamPrompt()
     prompt.prompt = '>->-> '
-    prompt.cmdloop(
-        '\nWelcome to Gilliam the JS class diagram drawer. \nType help or ? for a list of commands')
+    prompt.cmdloop('\nWelcome to Gilliam the JS class diagram drawer.\
+                    \nType help or ? for a list of commands')
+
